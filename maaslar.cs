32a6e33ac033e1c5fa87adde43e79da66621268b
Macksn <itsmacksn@yahoo.com>
2024-03-24 21:46:27 +0300

    Maas_Hesabi
    Class ile maaş hesabı

---
+++ maaslar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication9
+{
+    class maaslar
+    {
+        public int gun;
+        public int hesapla()
+        {
+            return (gun * 70);
+        }
+        
+    }
+}
+
